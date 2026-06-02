aebff36b6968a3a91bec03bc740c4725f3c99b8a
Maxington20 <max.herrington@gmail.com>
2026-06-02 11:41:58 -0400

    explosion on death

--- Assets/Scripts/Combat/DestroyAfterDelay.cs
+++ Assets/Scripts/Combat/DestroyAfterDelay.cs
@@ -3,9 +3,41 @@ using UnityEngine;
 public class DestroyAfterDelay : MonoBehaviour
 {
     [SerializeField] private float lifetime = 0.35f;
+    [SerializeField] private float growthSpeed = 5f;
 
-    private void Start()
+    private SpriteRenderer spriteRenderer;
+    private Color startColor;
+    private float elapsedTime;
+
+    private void Awake()
     {
-        Destroy(gameObject, lifetime);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            startColor = spriteRenderer.color;
+        }
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        transform.localScale += Vector3.one * growthSpeed * Time.deltaTime;
+
+        if (spriteRenderer != null)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / lifetime);
+
+            Color color = startColor;
+            color.a = alpha;
+
+            spriteRenderer.color = color;
+        }
+
+        if (elapsedTime >= lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
