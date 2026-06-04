b4ee79bc8891fdc1cc91327685350daf7788fd23
Maxington20 <max.herrington@gmail.com>
2026-06-04 13:34:22 -0400

    speed boost ui added.

--- Assets/Scripts/UI/PlayerStatusUI.cs
+++ Assets/Scripts/UI/PlayerStatusUI.cs
@@ -5,67 +5,84 @@ using UnityEngine.UI;
 public class PlayerStatusUI : MonoBehaviour
 {
     [SerializeField] private ShipHealth playerHealth;
+    [SerializeField] private BoostController boostController;
+
     [SerializeField] private TMP_Text shipNameText;
     [SerializeField] private Image shieldFillImage;
     [SerializeField] private Image hullFillImage;
+    [SerializeField] private Image boostFillImage;
 
     private void Start()
     {
-        if (playerHealth == null)
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerHealth == null && player != null)
         {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-            if (player != null)
+            playerHealth = player.GetComponent<ShipHealth>();
+        }
+
+        if (boostController == null && player != null)
+        {
+            boostController = player.GetComponent<BoostController>();
+        }
+
+        if (playerHealth != null)
+        {
+            playerHealth.ShieldChanged += HandleShieldChanged;
+            playerHealth.HullChanged += HandleHullChanged;
+
+            ShipDefinitionHolder holder = playerHealth.GetComponent<ShipDefinitionHolder>();
+            if (holder != null && holder.ShipDefinition != null && shipNameText != null)
             {
-                playerHealth = player.GetComponent<ShipHealth>();
+                shipNameText.text = holder.ShipDefinition.ShipName;
             }
+
+            HandleShieldChanged(playerHealth.CurrentShield, playerHealth.MaxShield);
+            HandleHullChanged(playerHealth.CurrentHull, playerHealth.MaxHull);
         }
 
-        if (playerHealth == null)
+        if (boostController != null)
         {
-            return;
+            boostController.EnergyChanged += HandleBoostChanged;
+            HandleBoostChanged(boostController.CurrentEnergy, boostController.MaxEnergy);
         }
-
-        playerHealth.ShieldChanged += HandleShieldChanged;
-        playerHealth.HullChanged += HandleHullChanged;
-
-        ShipDefinitionHolder holder = playerHealth.GetComponent<ShipDefinitionHolder>();
-        if (holder != null && holder.ShipDefinition != null && shipNameText != null)
-        {
-            shipNameText.text = holder.ShipDefinition.ShipName;
-        }
-
-        HandleShieldChanged(playerHealth.CurrentShield, playerHealth.MaxShield);
-        HandleHullChanged(playerHealth.CurrentHull, playerHealth.MaxHull);
     }
 
     private void OnDestroy()
     {
-        if (playerHealth == null)
+        if (playerHealth != null)
         {
-            return;
+            playerHealth.ShieldChanged -= HandleShieldChanged;
+            playerHealth.HullChanged -= HandleHullChanged;
         }
 
-        playerHealth.ShieldChanged -= HandleShieldChanged;
-        playerHealth.HullChanged -= HandleHullChanged;
+        if (boostController != null)
+        {
+            boostController.EnergyChanged -= HandleBoostChanged;
+        }
     }
 
     private void HandleShieldChanged(float currentShield, int maxShield)
     {
-        if (shieldFillImage == null || maxShield <= 0)
+        if (shieldFillImage != null && maxShield > 0)
         {
-            return;
+            shieldFillImage.fillAmount = currentShield / maxShield;
         }
-
-        shieldFillImage.fillAmount = currentShield / maxShield;
     }
 
     private void HandleHullChanged(int currentHull, int maxHull)
     {
-        if (hullFillImage == null || maxHull <= 0)
+        if (hullFillImage != null && maxHull > 0)
         {
-            return;
+            hullFillImage.fillAmount = (float)currentHull / maxHull;
         }
+    }
 
-        hullFillImage.fillAmount = (float)currentHull / maxHull;
+    private void HandleBoostChanged(float currentEnergy, float maxEnergy)
+    {
+        if (boostFillImage != null && maxEnergy > 0)
+        {
+            boostFillImage.fillAmount = currentEnergy / maxEnergy;
+        }
     }
 }
